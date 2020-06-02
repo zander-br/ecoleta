831f343100a0b4a16c1127c74bec63395bd66dc0
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-06-02 19:39:43 -0300

    feat: create route list items

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,18 @@
 import express from 'express';
 
+import knex from './database/connection';
+
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' })
+routes.get('/items', async (request, response) => {
+  const items = await knex('items').select('*');
+
+  const serializedItems = items.map(item => ({
+    ...item,
+    image_url: `http://localhost:3333/uploads/${item.image}`,
+  }));
+
+  return response.json(serializedItems);
 });
 
 export default routes;
