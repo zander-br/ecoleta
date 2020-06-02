502a1182a37e55f3d4585d250a8c486903808c8c
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-06-02 18:10:06 -0300

    refactor: create routes file

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 
+import routes from './routes';
+
 const app = express();
 
-app.get('/users', () => {
-  console.log('Listagem de usuários');
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
