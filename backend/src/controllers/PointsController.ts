9e2b95c9c50289ea027b2340d733b002313bb11d
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-06-02 20:39:49 -0300

    refactor: change image fake in create a point

--- backend/src/controllers/PointsController.ts
+++ backend/src/controllers/PointsController.ts
@@ -51,7 +51,7 @@ class PointsController {
       longitude, 
       city, 
       uf, 
-      image: 'image-fake',
+      image: 'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
     }
   
     const insertedIds = await trx('points').insert(point);
