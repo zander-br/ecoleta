d73ba61e5c534bc07f4c0e618e3478b4fcf60d89
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-06-04 17:38:19 -0300

    fix: insert ip in get items

--- backend/src/controllers/ItemsController.ts
+++ backend/src/controllers/ItemsController.ts
@@ -8,7 +8,7 @@ class ItemsController {
   
     const serializedItems = items.map(item => ({
       ...item,
-      image_url: `http://localhost:3333/uploads/${item.image}`,
+      image_url: `http://192.168.15.10:3333/uploads/${item.image}`,
     }));
   
     return response.json(serializedItems);
