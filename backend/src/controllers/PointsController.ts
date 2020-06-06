129d8d859e5adf031a9007a5caa8931d0f8bb348
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-06-06 14:21:40 -0300

    feat: add upload image in point controller

--- backend/src/controllers/PointsController.ts
+++ backend/src/controllers/PointsController.ts
@@ -51,14 +51,17 @@ class PointsController {
       longitude, 
       city, 
       uf, 
-      image: 'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+      image: request.file.filename,
     }
   
     const insertedIds = await trx('points').insert(point);
   
     const point_id = insertedIds[0]
   
-    const pointItems = items.map((item_id: number) => ({
+    const pointItems = items
+      .split(',')
+      .map((item: string) => Number(item.trim()))
+      .map((item_id: number) => ({
       item_id,
       point_id,
     }));
