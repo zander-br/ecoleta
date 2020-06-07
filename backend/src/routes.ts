272cb570d54f76c4d82f575cf7a86841f4157815
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-06-07 11:50:42 -0300

    feat: add validation in add points

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import multer from 'multer';
+import { celebrate, Joi } from 'celebrate';
 
 import multerConfig from './config/multer';
 
@@ -14,7 +15,22 @@ const itemsController = new ItemsController();
 
 routes.get('/items', itemsController.index);
 
-routes.post('/points', upload.single('image'), pointsController.create);
+routes.post(
+  '/points', 
+  upload.single('image'),
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      whatsapp: Joi.string().required(),
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required(),
+      uf: Joi.string().required().max(2),
+      city: Joi.string().required(),
+      items: Joi.string().required(),
+    }),
+  }), 
+  pointsController.create);
 routes.get('/points', pointsController.index);
 routes.get('/points/:id', pointsController.show);
 
